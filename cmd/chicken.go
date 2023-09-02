d33012d41c42c0881483e2bdbe32cc29dd294247
teke4869 <r08028099448@icloud.com>
2023-09-03 02:30:50 +0900

    💄 チキンが表示されるようにした

--- cmd/chicken.go
+++ cmd/chicken.go
@@ -9,6 +9,20 @@ import (
 
 	"github.com/spf13/cobra"
 )
+const chicken = 
+`    
+    　　　 　　(⌒⌒＼
+ほげー	　　|＼＞―、)
+	　＿|／・． ヽ
+	　＼/　　〇　|
+	　 ｜　　　　ヽノ⌒ヽ
+	　 ｜　 ￣￣ヽ　 ⌒ |
+	　 人　 ＼＿ノ　 /)ノ
+	　　 ＼　　　　 /
+	　　　 ＼＿＿_／
+	　　　 ＿∥＿∥
+	　　　　⌒　⌒`
+
 // chickenCmd represents the chicken command
 var chickenCmd = &cobra.Command{
 	Use:   "chicken",
@@ -19,7 +33,16 @@ and usage of using your command. For example:
 Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		m, _ := cmd.Flags().GetString("msg")
+		fmt.Println(chicken)
+		fmt.Println(m)
+	},
 }
+
 func init() {
 	rootCmd.AddCommand(chickenCmd)
+
+	chickenCmd.Flags().StringP("msg", "m", "", "message to print")
+
 }
