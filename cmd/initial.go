2a82e7bf735761ed50f0bd09a2adb3af24b64aeb
teke4869 <r08028099448@icloud.com>
2023-09-04 00:01:21 +0900

    ✨ initでREADME通りのディレクトリ構造を構築する機能を作成

--- cmd/initial.go
+++ cmd/initial.go
@@ -2,7 +2,7 @@ package cmd
 
 import (
 	"fmt"
-	//"os"
+	"os"
 	"github.com/spf13/cobra"
 )
 
@@ -10,23 +10,39 @@ import (
 var initCmd = &cobra.Command{
 	Use:   "init",
 	Short: "A brief description of your command",
-//├── part1
-        //│     └── 001
-        //│       ├── 001.md
-        //│       └── problems
-        //│             ├── 1.md
-        //│             └── 2.md
-		//上記のようなディレクトリ構造を作成する機能
+
 	Run: func(cmd *cobra.Command, args []string) {
-		// err := os.MkdirAll("part1/001/problems/1.md,2.md", 0777)
-		// if err != nil {
-		// 	fmt.Println(err)
-		// }
-		// i := os.MkdirAll("part1/001/001.md", 0777)
-		// if i != nil {
-		// 	fmt.Println(i)
-		// }
-		fmt.Println("init called")
+		str :="# 001\n\nここはmarkdownを自由に記述できる。\n\nここまで---\n\n## 問題\n\n## テストケース\n\n## 模範回答"
+		str2 :="# 001\n\nここはmarkdownを自由に記述できる。"
+		// ファイルを作成
+		 err := os.MkdirAll("part1/001/problems", 0777)
+		 if err != nil {
+		 	fmt.Println(err)
+		 }
+		 i := os.MkdirAll("part1/001", 0777)
+		 if i != nil {
+		 	fmt.Println(i)
+		 }
+		 fp, err := os.Create("part1/001/problems/1.md")
+    	 if err != nil {
+            fmt.Println(err)
+         }
+		 defer fp.Close()
+		 fp.Write([]byte(str))
+
+		 p, u := os.Create("part1/001/problems/2.md")
+    	 if u != nil {
+            fmt.Println(u)
+         }
+		 defer p.Close()
+		 p.Write([]byte(str))
+
+		 t, r := os.Create("part1/001/001.md")
+    	 if r != nil {
+            fmt.Println(r)
+         }
+		 defer t.Close()
+		 t.Write([]byte(str2))
 	},
 }
 
