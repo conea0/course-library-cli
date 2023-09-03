fde08f30889d6f60c51282ee47e5788e0b9ee477
teke4869 <r08028099448@icloud.com>
2023-09-03 21:28:04 +0900

    ♻️ ファイル名変更

--- cmd/initial.go
+++ cmd/initial.go
@@ -2,7 +2,7 @@ package cmd
 
 import (
 	"fmt"
-	"os"
+	//"os"
 	"github.com/spf13/cobra"
 )
 
@@ -18,14 +18,15 @@ var initCmd = &cobra.Command{
         //│             └── 2.md
 		//上記のようなディレクトリ構造を作成する機能
 	Run: func(cmd *cobra.Command, args []string) {
-		err := os.MkdirAll("part1/001/problems/1.md,2.md", 0777)
-		if err != nil {
-			fmt.Println(err)
-		}
-		i := os.MkdirAll("part1/001/001.md", 0777)
-		if i != nil {
-			fmt.Println(i)
-		}
+		// err := os.MkdirAll("part1/001/problems/1.md,2.md", 0777)
+		// if err != nil {
+		// 	fmt.Println(err)
+		// }
+		// i := os.MkdirAll("part1/001/001.md", 0777)
+		// if i != nil {
+		// 	fmt.Println(i)
+		// }
+		fmt.Println("init called")
 	},
 }
 
