a2f14b06aa84d784ad4eb9044a65a18343cccb43
shuya goto <f20065@g.ichinoseki.ac.jp>
2023-09-06 19:42:17 +0900

    静的ファイルもビルドするよう対応

--- cmd/generate.go
+++ cmd/generate.go
@@ -1,24 +1,31 @@
 package cmd
 
 import (
+	_ "embed"
 	"fmt"
 	"os"
 	"path/filepath"
-	"io"
 
 	"github.com/spf13/cobra"
 )
 
 const (
+	// embedを指定した場所からの相対パス
 	problemTemplatePath = "template/problem.md"
-	textTemplatePath = "template/text.md"
+	textTemplatePath    = "template/text.md"
 )
+
+//go:embed template/problem.md
+var problemTemplate string
+
+//go:embed template/text.md
+var textTemplate string
 
 // generateCmd represents the generate command
 var generateCmd = &cobra.Command{
 	Use:   "generate",
 	Short: "コース作成に必要なファイルを生成します。",
-	Run: generate,
+	Run:   generate,
 }
 
 func init() {
@@ -28,7 +35,6 @@ func init() {
 	generateCmd.Flags().IntP("num", "n", 1, "演習問題の個数を指定します。")
 	generateCmd.Flags().StringP("path", "p", "./", "生成先を指定します。")
 }
-
 
 func generate(cmd *cobra.Command, args []string) {
 	numProblems, outputDirPath, problemsOutputDirPath := getGenCmdArgs(cmd)
@@ -48,59 +54,19 @@ func generate(cmd *cobra.Command, args []string) {
 	}
 
 	// 問題を生成する
-	problemTemplate := readProblemTemplates()
 	for i := 0; i < numProblems; i++ {
-		generateProblem(problemsOutputDirPath, problemTemplate, i + 1)
+		generateProblem(problemsOutputDirPath, problemTemplate, i+1)
 	}
 
 	fmt.Printf("%d個の問題を生成しました。\n", numProblems)
 	fmt.Printf("生成先: %s\n", outputDirPath)
 }
 
-// 問題テンプレートファイルを読み込んで文字列を返す
-func readProblemTemplates() (string) {
-	problemString, err := makeFileContentToString(problemTemplatePath)
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
-	}
-
-	return problemString
-}
-
-// 資料テンプレートファイルを読み込んで文字列を返す
-func readTextTemplates() (string) {
-	textString, err := makeFileContentToString(textTemplatePath)
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
-	}
-
-	return textString
-}
-
-// ファイルの内容を文字列にして返す
-func makeFileContentToString(path string) (string, error) {
-	file, err := os.Open(path)
-	if err != nil {
-		return "", err
-	}
-
-	defer file.Close()
-
-	content, err := io.ReadAll(file)
-	if err != nil {
-		return "", err
-	}
-
-	return string(content), nil
-}
-
 // 問題を生成する
 func generateProblem(path string, problemTemplate string, num int) {
 	// テンプレートファイルを読み込む
 
-	fileName := fmt.Sprintf("%s/%v.md", path , num)
+	fileName := fmt.Sprintf("%s/%v.md", path, num)
 
 	// ファイルを作成
 	problemFile, err := os.Create(fileName)
@@ -118,12 +84,6 @@ func generateProblem(path string, problemTemplate string, num int) {
 
 // 資料を生成する
 func generateText(dirPath string) error {
-	// テンプレートファイルを読み込む
-	textString, err := makeFileContentToString(textTemplatePath)
-	if err != nil {
-		return err
-	}
-
 	// ファイルを作成
 	outputPath := filepath.Join(dirPath, "text.md")
 	textFile, err := os.Create(outputPath)
@@ -133,7 +93,7 @@ func generateText(dirPath string) error {
 	defer textFile.Close()
 
 	// ファイルに書き込む
-	fmt.Fprintf(textFile, textString)
+	fmt.Fprintf(textFile, textTemplate)
 
 	return nil
 }
