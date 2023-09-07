f151908c226fe9c58d77cdbb2f32348f28d939e2
teke4869 <r08028099448@icloud.com>
2023-09-08 02:50:30 +0900

    🎨 generateと同様のコード構造に改善

--- cmd/initial.go
+++ cmd/initial.go
@@ -1,51 +1,85 @@
 package cmd
 
 import (
+	_"embed" 
 	"fmt"
 	"os"
+
 	"github.com/spf13/cobra"
 )
+
+const (
+	// embedを指定した場所からの相対パス
+	problemTemplatePath = "template/problem.md"
+	textTemplatePath    = "template/text.md"
+)
+
+//go:embed template/problem.md
+var problemsTemplate string
+
+//go:embed template/text.md
+var textTemplate string
 
 // initCmd represents the init command
 var initCmd = &cobra.Command{
 	Use:   "init",
-	Short: "A brief description of your command",
-
-	Run: func(cmd *cobra.Command, args []string) {
-		str :="template/problem.md"
-		str2 :="template/text.md"
-		// ファイルを作成
-		 err := os.MkdirAll("part1/001/problems", 0777)
-		 if err != nil {
-		 	fmt.Println(err)
-		 }
-		 i := os.MkdirAll("part1/001", 0777)
-		 if i != nil {
-		 	fmt.Println(i)
-		 }
-		 fp, err := os.Create("part1/001/problems/1.md")
-    	 if err != nil {
-            fmt.Println(err)
-         }
-		 defer fp.Close()
-		 fp.Write([]byte(str))
-
-		 p, u := os.Create("part1/001/problems/2.md")
-    	 if u != nil {
-            fmt.Println(u)
-         }
-		 defer p.Close()
-		 p.Write([]byte(str))
-
-		 t, r := os.Create("part1/001/001.md")
-    	 if r != nil {
-            fmt.Println(r)
-         }
-		 defer t.Close()
-		 t.Write([]byte(str2))
-	},
-}
-
+	Short: "初期セットアップ",
+	Run: initial,
+} 
+	
 func init() {
 	rootCmd.AddCommand(initCmd)
 }
+	
+func initial(cmd *cobra.Command, args []string) {
+	// ディレクトリを作成
+	err := os.MkdirAll("part1/001/problems", 0777)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	//資料を生成
+	err = generateText("part1/001")
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	// 問題を生成
+	for i := 0; i < 2; i++ {
+		generateProblem("part1/001/problems", problemsTemplate, i+1)
+	}
+
+
+	fmt.Printf("complete!")
+}
+
+// generateText 資料を生成する
+func generateText(dir string) error {
+	// ファイルを作成
+	f, err := os.Create("part1/001/001.md")
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+	// テンプレートを埋め込む
+	fmt.Fprint(f, textTemplate)
+
+	return nil
+}
+
+// generateProblem 問題を生成する
+func generateProblem(path string, template string, num int) error {
+	// ファイルを作成
+	f, err := os.Create(fmt.Sprintf("%s/%03d.md", path, num))
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+	// テンプレートを埋め込む
+	fmt.Fprint(f, template)
+
+	return nil
+}
+
