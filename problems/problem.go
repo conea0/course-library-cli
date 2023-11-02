f05085d7c81628ead8466554ad55fb38923d3b96
shuya goto <f20065@g.ichinoseki.ac.jp>
2023-11-02 12:02:32 +0900

    mdをパースする用の構造体を定義

--- problems/problem.go
+++ problems/problem.go
@@ -13,3 +13,25 @@ type Problem struct {
 	Code      string
 }
 
+
+type MdPrefix string
+
+type ReadBlockFn func(*Problem) error
+
+const (
+	StatementPre = "## 問題"
+	TestcasePre  = "## テストケース"
+	CodePre      = "## 模範回答"
+)
+
+type Md struct {
+	s []string
+
+	mdPrefixFns map[MdPrefix]ReadBlockFn
+
+	curr int
+	next int
+
+	errors []error
+}
+
