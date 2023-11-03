a5d2bc3456784a35a4d87f16ae140086fa79bf2e
shuya goto <f20065@g.ichinoseki.ac.jp>
2023-11-04 03:06:55 +0900

    初期化とmdのパスを取得する関数の実装

---
+++ cmd/output.go
@@ -0,0 +1,74 @@
+package cmd
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+
+	"github.com/spf13/cobra"
+)
+
+// outputCmd represents the output command
+var outputCmd = &cobra.Command{
+	Use:   "output",
+	Short: "inputを模範解答のコードで評価し、outputを出力します",
+	Run:   output,
+}
+
+func init() {
+	rootCmd.AddCommand(outputCmd)
+
+	outputCmd.Flags().StringP("path", "p", "./", "パスを指定します")
+}
+
+func output(cmd *cobra.Command, args []string) {
+	path, err := cmd.Flags().GetString("path")
+	if err != nil {
+		fmt.Fprint(os.Stderr, "実行失敗: %w", err)
+	}
+
+	paths, err := getMdPaths(path)
+	if err != nil {
+		fmt.Fprint(os.Stderr, "実行失敗: %w", err)
+	}
+
+	outDir := getOutDir(paths[0])
+}
+
+// 指定されたディレクトリから.mdのファイルを抽出してフルパスを取得
+func getMdPaths(path string) ([]string, error) {
+	var filenames []string
+
+	// mdファイルが指定された場合はそのまま返す
+	ex := filepath.Ext(path)
+	if ex == "md" {
+		filenames = append(filenames, path)
+		return filenames, nil
+	}
+
+	files, err := os.ReadDir(path)
+	if err != nil {
+		return []string{}, fmt.Errorf("ファイルが得られませんでした: %w", err)
+	}
+
+	for _, f := range files {
+		// mdファイルでない場合はスキップ
+		ext := filepath.Ext(f.Name())
+		if ext != "md" {
+			continue
+		}
+
+		fullpath := filepath.Join(path, f.Name())
+		filenames = append(filenames, fullpath)
+	}
+
+	if len(files) == 0 {
+		return []string{}, fmt.Errorf("指定されたディレクトリにファイルが存在しません")
+	}
+
+	return filenames, nil
+}
+
+func getOutDir(mdPath string) string {
+	return filepath.Join(filepath.Dir(mdPath), "out")
+}
