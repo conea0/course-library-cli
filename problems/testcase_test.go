1b162f2fbcfa102a483494f340df64aa2dccb9dc
shuya goto <f20065@g.ichinoseki.ac.jp>
2023-11-04 03:22:36 +0900

    コメント追加

--- problems/testcase_test.go
+++ problems/testcase_test.go
@@ -18,6 +18,8 @@ func TestTestCaseFromJSON(t *testing.T) {
 		},
 		{
 			name: "valid json5",
+
+			// 末尾にコンマ
 			json: `{"tests": [{"input": ["1 2 3", "3 4 5",],}, {"input": ["a b c", "aa bb cc",],, "output": "some_output_value_2",},],}`,
 		},
 	}
